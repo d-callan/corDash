d84a056cdcf78aea3c77705b77c9e4749171bad6
Danielle Callan <dcallan@upenn.edu>
2023-12-12 23:00:21 -0500

    wip improving the skeleton of the app

--- src/app.py
+++ src/app.py
@@ -7,39 +7,77 @@ import base64
 app = dash.Dash(__name__)
 
 # Layout of the app
-app.layout = html.Div([
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        multiple=False
-    ),
-    html.Div(id='output-data')
-])
-
-# Callback to display the contents of the uploaded file
-@app.callback(Output('output-data', 'children'),
-              [Input('upload-data', 'contents')])
-def update_output(contents):
-    if contents is not None:
-        content_type, content_string = contents.split(',')
-        decoded = base64.b64decode(content_string).decode('utf-8')
-        return html.Div([
-            html.H5('Uploaded File Content:'),
-            html.Pre(decoded)
-        ])
+app.layout = html.Div(
+    children=[
+        html.H1('Correlation Matrix as Network Visualization'),
+        html.Div(
+            children=[
+                # Side panel content here
+                html.Label('Upload Data Table', style={'font-weight': 'bold'}),
+                dcc.Upload(
+                    id='upload-data',
+                    children=html.Div([
+                        'Drag and Drop or ',
+                        html.A('Select Files')
+                    ]),
+                    style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '10px'
+                    },
+                    multiple=False
+                ),
+                html.P(),
+                html.Label('Upload Second Data Table (optional)', style={'font-weight': 'bold'}),
+                dcc.Upload(
+                    id='upload-data2',
+                    children=html.Div([
+                        'Drag and Drop or ',
+                        html.A('Select Files')
+                    ]),
+                    style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '10px'
+                    },
+                    multiple=False
+                )
+            ],
+            style={
+                'width': '20%',  # adjust the width as needed
+                'float': 'left',  # float the side panel to the left
+                'padding': '20px'  # add padding for spacing
+            }
+        ),
+        html.Div(
+            children=[
+                dcc.Tabs(
+                    id='tabs',
+                    children=[
+                        dcc.Tab(label='Network', value='network'),
+                        dcc.Tab(label='Table', value='table')
+                    ]
+                ),
+                html.Div(id='tab-content')
+            ],
+            style={
+                'width': '70%',  # adjust the width as needed
+                'float': 'right',  # float the main content to the right
+                'padding': '20px'  # add padding for spacing
+            }
+        )
+    ]
+)
 
 # Run the app
 if __name__ == '__main__':
