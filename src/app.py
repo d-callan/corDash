af4e8299684bad3f0cc21468fa5a2d824697ef1d
Danielle Callan <dcallan@upenn.edu>
2023-12-14 22:22:27 -0500

    wip some helper functions drafted

--- src/app.py
+++ src/app.py
@@ -2,8 +2,9 @@ import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output
 import base64
-import pandas as pd
 from scipy.stats import pearsonr, spearmanr
+import pandas as pd
+import numpy as np
 import networkx as nx
 import logging
 import io
@@ -91,6 +92,131 @@ app.layout = html.Div(
         )
     ]
 )
+
+def load_data(contents, filename):
+    """
+    Load the uploaded data.
+
+    Parameters:
+    - contents (str): The contents of the uploaded file.
+    - filename (str): The name of the uploaded file.
+
+    Returns:
+    - pandas.DataFrame: The loaded data.
+    """
+    content_type, content_string = contents.split(',')
+    decoded_contents = base64.b64decode(content_string)
+    return pd.read_table(io.StringIO(decoded_contents.decode('utf-8')))
+
+def correlation_matrix(df, df2=None):
+    """
+    Calculate the correlation matrix.
+
+    Parameters:
+    - df (pandas.DataFrame): The DataFrame to calculate the correlation matrix from.
+
+    Returns:
+    - pandas.DataFrame: The correlation matrix.
+    """
+    if df2 is not None:
+        df = pd.concat([df, df2])
+    # Calculate the correlation matrix with pvalues
+    p_values_matrix = df.apply(lambda col: df.apply(lambda col2: pearsonr(col, col2)[1]))
+    correlation_matrix = df.apply(lambda col: df.apply(lambda col2: pearsonr(col, col2)[0]))
+    # if df2 is none, find upper triangle of correlation matrix without diagonal
+    if df2 is None:
+        correlation_matrix = correlation_matrix.where(np.triu(np.ones(correlation_matrix.shape), k=1).astype(np.bool))
+        p_values_matrix = p_values_matrix.where(np.triu(np.ones(p_values_matrix.shape), k=1).astype(np.bool))
+    # if df2 is not none, find pairwise correlation between df and df2
+    else:
+        correlation_matrix = correlation_matrix.where(np.triu(np.ones(correlation_matrix.shape), k=1).astype(np.bool))
+        p_values_matrix = p_values_matrix.where(np.triu(np.ones(p_values_matrix.shape), k=1).astype(np.bool))
+        
+    return correlation_matrix, p_values_matrix
+
+def matrix_to_edgelist(correlation_matrix, p_values_matrix):
+    """
+    Convert the correlation matrix to an edgelist.
+
+    Parameters:
+    - correlation_matrix (pandas.DataFrame): The correlation matrix.
+
+    Returns:
+    - pandas.DataFrame: The edgelist.
+    """
+    edge_list = correlation_matrix.stack().reset_index()
+    edge_list.columns = ['source', 'target', 'correlation']
+    edge_list['p_value'] = p_values_matrix.stack().reset_index()['level_2']
+    return edge_list
+
+def generate_table(df):
+    """
+    Generate a table from a Pandas DataFrame.
+
+    Parameters:
+    - df (pandas.DataFrame): The DataFrame to generate the table from.
+
+    Returns:
+    - html.Table: The table.
+    """
+    return html.Table(
+        children=[
+            html.Thead(
+                html.Tr(
+                    children=[
+                        html.Th(col) for col in df.columns
+                    ]
+                )
+            ),
+            html.Tbody(
+                [
+                    html.Tr(
+                        children=[
+                            html.Td(df.iloc[i][col]) for col in df.columns
+                        ]
+                    ) for i in range(len(df))
+                ]
+            )
+        ]
+    )
+
+def generate_network(edge_list):
+    """
+    Generate a network from a Pandas DataFrame.
+
+    Parameters:
+    - edge_list (pandas.DataFrame): The edge list containing columns 'source', 'target', 'correlation' and 'p_value'.
+
+    Returns:
+    - networkx.Graph: The network.
+    """
+    G = nx.from_pandas_edgelist(edge_list, source='source', target='target', edge_attr=['correlation', 'p_value'])
+    # color edges by whether the correlation is positive or negative
+    for edge in G.edges:
+        if G[edge[0]][edge[1]]['correlation'] > 0:
+            G[edge[0]][edge[1]]['color'] = 'green'
+        else:
+            G[edge[0]][edge[1]]['color'] = 'red'
+    # set edge thickness based on absolute value of correlation
+    for edge in G.edges:
+        G[edge[0]][edge[1]]['width'] = abs(G[edge[0]][edge[1]]['correlation'])
+    # add a label to each node
+    for node in G.nodes:
+        G.nodes[node]['label'] = node
+    # add a legend to the network for color
+    G.graph['legend'] = [
+        {
+            'color': 'green',
+            'label': 'Positive Correlation'
+        },
+        {
+            'color': 'red',
+            'label': 'Negative Correlation'
+        }
+    ]
+
+    return G
+
 
 @app.callback(
     dash.dependencies.Output('tab-content', 'children'),
